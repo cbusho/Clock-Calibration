2ae9553393e98111866a9f0721203d61d07a1c75
Colin Busho <C15Colin.Busho@C15CRBusho.usafa.ds.af.edu>
2013-10-25 09:14:52 -0600

    helper and implementation to set the clock of the MSP430

---
+++ clock_calibration.c
@@ -0,0 +1,8 @@
+/*
+ * clock_calibration.c
+ *
+ *  Created on: Oct 24, 2013
+ *      Author: C15Colin.Busho
+ */
+
+
---
+++ clock_calibration.h
@@ -0,0 +1,13 @@
+/*
+ * clock_calibration.h
+ *
+ *  Created on: Oct 24, 2013
+ *      Author: C15Colin.Busho
+ */
+
+#ifndef CLOCK_CALIBRATION_H_
+#define CLOCK_CALIBRATION_H_
+
+
+
+#endif /* CLOCK_CALIBRATION_H_ */
