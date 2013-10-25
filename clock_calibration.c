22b15d1aeb7eab39c7519ca8ad43451e41d7f480
Colin Busho <C15Colin.Busho@C15CRBusho.usafa.ds.af.edu>
2013-10-25 09:22:04 -0600

    initial files were empty just saved my work

--- clock_calibration.c
+++ clock_calibration.c
@@ -5,4 +5,29 @@
  *      Author: C15Colin.Busho
  */
 
+#include "clock_calibration.h"
+#include "msp430.h"
 
+void clockSet1MHZ(){
+	DCOCTL = 0x00;
+	BCSCTL1 = CALBC1_1MHZ;		// Set clockspeed to 1MHZ
+	DCOCTL = CALDCO_1MHZ;
+}
+
+void clockSet8MHZ(){
+	DCOCTL = 0x00;
+	BCSCTL1 = CALBC1_8MHZ;		// Set clockspeed to 8MHZ
+	DCOCTL = CALDCO_8MHZ;
+}
+
+void clockSet12MHZ(){
+	DCOCTL = 0x00;
+	BCSCTL1 = CALBC1_12MHZ;		// Set clockspeed to 12MHZ
+	DCOCTL = CALDCO_12MHZ;
+}
+
+void clockSet16MHZ(){
+	DCOCTL = 0x00;
+	BCSCTL1 = CALBC1_16MHZ;		// Set clockspeed to 16MHZ
+	DCOCTL = CALDCO_16MHZ;
+}
--- clock_calibration.h
+++ clock_calibration.h
@@ -8,6 +8,9 @@
 #ifndef CLOCK_CALIBRATION_H_
 #define CLOCK_CALIBRATION_H_
 
-
+void clockSet1MHZ();
+void clockSet8MHZ();
+void clockSet12MHZ();
+void clockSet16MHZ();
 
 #endif /* CLOCK_CALIBRATION_H_ */
